34b809eac0f2e0f55443bf496c6c8073c330372b
夏目 <your.email@example.com>
2022-03-20 04:36:44 +0900

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  root 'home_pages#home'
-  get 'home_pages/top'
-  get 'home_pages/about'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'home_pages#top'
+  get  '/about',   to: 'home_pages#about'
 end
--- test/controllers/home_pages_controller_test.rb
+++ test/controllers/home_pages_controller_test.rb
@@ -1,20 +1,19 @@
 require 'test_helper'
 
 class HomePagesControllerTest < ActionDispatch::IntegrationTest
-
   def setup
-    @base_title = "Card Match"
+    @base_title = 'Card Match'
   end
 
   test 'should get top' do
-    get home_pages_top_url
+    get root_path
     assert_response :success
-    assert_select "title", "トップページ | #{@base_title}"
+    assert_select 'title', "トップページ | #{@base_title}"
   end
 
   test 'should get about' do
-    get home_pages_about_url
+    get about_path
     assert_response :success
-    assert_select "title", "使い方 | #{@base_title}"
+    assert_select 'title', "使い方 | #{@base_title}"
   end
 end
