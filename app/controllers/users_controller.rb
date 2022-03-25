7b04557dac9899ba027c87912ac8bc8936c59187
夏目 <your.email@example.com>
2022-03-26 01:18:56 +0900

    プロフィール編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,9 +18,33 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = 'プロフィールの更新に成功しました'
+      redirect_to @user
+    else
+      flash.now[:danger] = 'プロフィールの編集に失敗しました'
+      redirect_to edit_user_path(@user)
+    end
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:warning] = 'ログインしてください'
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
   end
 end
