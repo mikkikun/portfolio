a6015b2115f3a0b2f4cab005430d327efe03095b
夏目 <your.email@example.com>
2022-03-26 03:48:07 +0900

    パスワード再設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
 
   before_save :downcase_email
   before_create :create_activation_digest
@@ -19,8 +19,24 @@ class User < ApplicationRecord
   validates :password,
             presence: true,
             length: { minimum: 6 }
+  
+            
+            
+            
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_columns(reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now)
+  end
+          
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+    end
 
-  private
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+  
+private
 
   def create_activation_digest
     self.activation_token = User.new_token
@@ -37,9 +53,22 @@ class User < ApplicationRecord
   end
 
   class << self
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                    BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+
     def new_token
       SecureRandom.urlsafe_base64
     end
+  end
+
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
 
     def digest(string)
       cost = if ActiveModel::SecurePassword.min_cost
@@ -54,13 +83,6 @@ class User < ApplicationRecord
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
-  end
-
-  def authenticated?(attribute, token)
-    digest = send("#{attribute}_digest")
-    return false if digest.nil?
-
-    BCrypt::Password.new(digest).is_password?(token)
   end
 
   def forget
