291d548413d8000ecad0ef344b08af56a2e7dff6
夏目 <your.email@example.com>
2022-03-26 06:18:37 +0900

    kihonn

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets, only: %i[new create edit update]
+  resources :posts, only: %i[create edit update destroy]
 end
