d982490b7e3f93253e79dc9aa6b9320abed0839a
夏目 <your.email@example.com>
2022-03-29 05:35:46 +0900

    画像投稿修正前

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,5 @@
 class MicropostsController < ApplicationController
-  before_action :logged_in_user, only: %i[create edit update destroy]
+  before_action :logged_in_user, only: %i[create edit update destroy new]
   before_action :correct_user, only: :destroy
 
   def new
