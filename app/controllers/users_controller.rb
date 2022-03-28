694482370b2d3fed6af949be6c389de38fc16eb9
夏目 <your.email@example.com>
2022-03-29 07:45:24 +0900

    チュートリアル14章ユーザ出てこない

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: %i[show edit update]
+  before_action :logged_in_user, only: %i[show edit update following followers]
   before_action :correct_user, only: %i[show edit update]
 
   def new
@@ -33,6 +33,18 @@ class UsersController < ApplicationController
       flash.now[:danger] = 'プロフィールの編集に失敗しました'
       render 'edit'
     end
+  end
+
+  def following
+    @title = 'Following'
+    @user  = User.find(params[:id])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = 'Followers'
+    @user  = User.find(params[:id])
+    render 'show_follow'
   end
 
   private
--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,6 @@ module Pf
     config.load_defaults 6.1
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
