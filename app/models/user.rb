e9f7aa21ec1a5960bb03966360507f744aef3637
夏目 <your.email@example.com>
2022-03-30 05:00:39 +0900

    a

--- app/models/user.rb
+++ app/models/user.rb
@@ -83,6 +83,13 @@ class User < ApplicationRecord
     reset_sent_at < 2.hours.ago
   end
 
+  def feed
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
+  end
+
   # ユーザーをフォローする
   def follow(other_user)
     following << other_user
